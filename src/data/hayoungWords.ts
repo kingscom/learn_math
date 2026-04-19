2f227065fa2f50cedb8349859a0dd317230e2dc1
kmw <kingskkk@nate.com>
2026-04-19 20:18:35 +0900

    하영이 영어 게임 추가10

--- src/data/hayoungWords.ts
+++ src/data/hayoungWords.ts
@@ -2,20 +2,14 @@ import { WordProblem } from '../types';
 
 // 하영이 영어 단어들
 export const hayoungWords: WordProblem[] = [
-  { korean: '연습/연습하다', english: 'practice' },
-{ korean: '맛있는', english: 'delicious' },
-{ korean: '날았다', english: 'flew' },
-{ korean: '현재의/선물', english: 'present' },
-{ korean: '조금', english: 'bit' },
-{ korean: '먹이를 주다', english: 'feed' },
-{ korean: '~을 자랑으로 여기다', english: 'be proud of' },
-{ korean: '도둑', english: 'thief' },
-{ korean: '오스트레일리아/호주', english: 'Australia' },
-{ korean: '매우 좋아하는', english: 'favorite' },
-{ korean: '선반', english: 'shelf' },
-{ korean: '야구', english: 'baseball' },
-{ korean: '가져오다', english: 'bring' },
-{ korean: '무거운', english: 'heavy' },
-{ korean: '아메리카/미국', english: 'America' },
-{ korean: '잔디', english: 'lawn' }
+  { korean: '아주 멋진', english: 'wonderful' },
+{ korean: '오랫동안', english: 'for a long time' },
+{ korean: '흥분시키다', english: 'excite' },
+{ korean: '아침', english: 'breakfast' },
+{ korean: '걸다', english: 'hang' },
+{ korean: '도착하다', english: 'arrive' },
+{ korean: '아름다운', english: 'beautiful' },
+{ korean: '과학', english: 'science' },
+{ korean: '잡고 있다', english: 'hold' },
+{ korean: '사진', english: 'picture' }
 ];
